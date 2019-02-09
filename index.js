3c4f7f4e3c3c23eb057110817f985e0ff1e0f381
Ranen Swilling <swillingray@gmail.com>
2019-02-09 22:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Su") {
+  console.log("THe name is correct")
+ } else { 
+    console.log("wrong")
+}
 
 // Don't worry about this
 module.exports = { name, height
